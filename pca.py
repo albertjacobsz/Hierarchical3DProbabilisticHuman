1db8c68b4f7dbdb1e0251e3a936ab08593e29516
albertjacobsz <dekater403@gmail.com>
2022-07-23 18:27:36 +0900

    pca

---
+++ pca.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.preprocessing import scale
+from sklearn import model_selection
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LinearRegression
+from sklearn.cross_decomposition import PLSRegression, PLSSVD
+from sklearn.metrics import mean_squared_error
+
+df = pd.read_csv('./csv_files/all_data.csv')
+df.info()
+
+y = df['Average Uncertainty']
+print(y)
