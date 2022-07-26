35ee5186d9790f07657c6aaf66e019355e173be2
albertjacobsz <dekater403@gmail.com>
2022-07-26 10:30:06 +0900

    change

--- HierProbHuman/HierarchicalProbabilistic3DHuman/pca.py
+++ HierProbHuman/HierarchicalProbabilistic3DHuman/pca.py
@@ -17,11 +17,9 @@ unique_corr_pairs = upper_corr_mat.unstack().dropna()
 
 # Sort correlation pairs
 sorted_mat = unique_corr_pairs.sort_values()
+print(type(sorted_mat))
 header = ['x', 'y', 'z']
-print(sorted_mat)
 with open('correlations.csv', 'a', newline='') as f:
     writer = csv.writer(f)
     # write the header
     writer.writerow(header)
-    for x in sorted_mat:
-        writer.writerow(x)
